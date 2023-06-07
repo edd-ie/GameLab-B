602fc8b1ca32e0ed55d4ba65b3bd6956bef5bec0
edd-ie <ogendoeddie@gmail.com>
2023-06-07 14:57:55 +0300

    Jump methods

--- app/models/Jump.rb
+++ app/models/Jump.rb
@@ -1,3 +1,16 @@
 class Jump < ActiveRecord::Base
     belongs_to :users
+
+    def self.lead
+        leads = Jump.order(score: :desc).limit(10)
+        leads.map { |user| [User.find(user.user_id).name, user.score] }
+    end
+
+    def self.rank(id)
+        Jump.order(score: :desc).find_index { |user| user.id == id } + 1
+    end
+
+    def self.games(id)
+        Jump.where(user_id: id).count
+    end
 end
